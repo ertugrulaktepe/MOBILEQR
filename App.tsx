e11ea1d3d158b622bb51a5d5a3ed83dce64c4e69
ertugrulaktepe <ertugrulaktepe71@gmail.com>
2023-08-11 23:46:49 +0300

    update splash screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Route from './src/routes/Route';
 import {AuthProvider} from './src/context/AuthContext';
@@ -6,13 +6,38 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {PaperProvider, DefaultTheme, useTheme} from 'react-native-paper';
-import {theme} from './src/app/configurations/theme';
+
+import {Dimensions, Image, Text} from 'react-native';
+import Video from 'react-native-video';
 
 function App(): JSX.Element {
+  const [splash, setSplash] = useState<boolean>(true);
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      setSplash(false);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+  if (splash) {
+    return (
+      <Video
+        source={require('./src/assets/splash.mp4')}
+        resizeMode="cover"
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }}
+      />
+    );
+  }
   return (
     <AuthProvider>
       <GestureHandlerRootView style={{flex: 1}}>
-        <PaperProvider theme={theme}>
+        <PaperProvider
+          theme={{
+            ...DefaultTheme,
+          }}>
           <NavigationContainer>
             <Route />
           </NavigationContainer>
--- src/app/configurations/theme.ts
+++ src/app/configurations/theme.ts
@@ -1,33 +1,31 @@
-import {Platform} from 'react-native';
-import {DefaultTheme, MD3LightTheme, configureFonts} from 'react-native-paper';
-import {Fonts, ThemeProp} from 'react-native-paper/lib/typescript/types';
-
-const fontConfig = {
-  bodyLarge: {
-    letterSpacing: 0.5,
-    lineHeight: 22,
-    fontSize: 20,
-    fontFamily: 'Poppins-Regular',
-  },
-  bodyMedium: {
-    letterSpacing: 0.25,
-    lineHeight: 20,
-    fontSize: 16,
-    fontFamily: 'Poppins-Medium',
-  },
-  bodySmall: {
-    letterSpacing: 0.25,
-    lineHeight: 20,
-    fontSize: 14,
-    fontFamily: 'Poppins-Regular',
-  },
-};
-
-export const theme: ThemeProp = {
-  ...MD3LightTheme,
-  fonts: configureFonts({config: fontConfig}),
+const theme = {
   colors: {
-    ...MD3LightTheme.colors,
-    primary: '#FFC700',
+    orange100: '#f8cd80',
+    orange200: '#ffcd82',
+    orange300: '#ffb890',
+    orange400: '#ff9c54',
+    orange500: '#ff7122',
+    orange600: '#e65a00',
+    blue800: '#0A0be2',
+    blue600: '#334b71',
+    gray400: '#455a64',
+    purple: '#373463',
+    pink: '#ff6584',
+    textColor: '#000000',
+    textColor600: '#444444',
+    textColor500: '#888888',
+    textColor400: '#AAAAAA',
+  },
+  fonts: {
+    fontFamily: 'Poppins, sans-serif',
+    fontFamilyMedium: 'Poppins-Medium, sans-serif',
+    fontFamilySemiBold: 'Poppins-SemiBold, sans-serif',
+    fontFamilyRegular: 'Poppins-Regular, sans-serif',
+  },
+  fontSizes: {
+    h1: 16,
+    h2: 14,
+    h3: 13,
   },
 };
+export default theme;
